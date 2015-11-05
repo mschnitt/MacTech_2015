0181f411067ea306bf2d983346d0e5c40e0d008b
mschnitt <mschnitt@yahoo.com>
2015-11-05 08:31:40 -0800

    Update open_command.sh

--- open_command.sh
+++ open_command.sh
@@ -5,7 +5,7 @@ open /usr/bin
 open /Applications/Utilities/Console.app
 open http://www.disneyanimation.com
 open /Library/Desktop\ Pictures/Frog.jpg
-open “/Library/Desktop Pictures/Yosemite.jpg”
+open "/Library/Desktop Pictures/Yosemite.jpg"
 open -a safari http://www.google.com
 open -a "google chrome" http://disneycareers.com/en/default/
 open http://disneyanimation.com
